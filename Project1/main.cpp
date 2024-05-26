cadf465a0de09b93b19b8154f43a447a7bebc49f
Noa Halali <noaha@mta.ac.il>
2024-05-26 18:05:31 +0300

    Added instructions

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,13 +3,20 @@
 #include "Graph.h"
 using namespace std; 
 
-
+/*This is a program that getting a directed graph data, and build its supergraph in a linear complexity.
+* At start, should enter: 
+1. integer number of vertices - n
+2. integer number of edges -m
+3. Couples of integers, each couple in a different line,
+   while each couple represents name of 2 vertices (each vertex is an integer between 1 and n)
+   the couple i,j will represents the edge (i,j)
+*/
 void main()
 {
 	int numVertices;
 	int numEdges;
 	Graph graph, transpose;
-
+	//להוריד את ההדפסות אח"כ
 	cout << "Enter number of vertices: ";
 	cin >> numVertices;
 	cout << "Enter number of edges: ";
